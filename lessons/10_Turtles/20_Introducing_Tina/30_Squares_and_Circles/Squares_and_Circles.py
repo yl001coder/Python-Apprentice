6ef01d474872a631e503d5642fa495ba2cc462bf
Voidoid1977 <jaysausa@student.purdueglobal.edu>
2025-09-04 14:14:44 -0400

    Typo: changed 'The' to 'This'

--- lessons/10_Turtles/20_Introducing_Tina/30_Squares_and_Circles/Squares_and_Circles.py
+++ lessons/10_Turtles/20_Introducing_Tina/30_Squares_and_Circles/Squares_and_Circles.py
@@ -1,5 +1,5 @@
 """
-The is a simple Turtle program that draws a square and writes a message. The
+This is a simple Turtle program that draws a square and writes a message. The
 lines that start with a # are comments. They are not executed by Python. The
 lines inside the three quotes are also comments, but of a different sort (
 called "doc comments" ) Comments are used to explain what the code does. Read
