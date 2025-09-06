0e6c36e1995c4ad13dc3da5c70e33cd427886484
dotnetdork <jaysausa@student.purdueglobal.edu>
2025-09-06 11:38:44 -0400

    Changed 'Leaguebot' to 'LeagueBot'; changed 'leaguebolt_bot.gif' to 'leaguebot_bot.gif'; fixed spacing for consistency

--- lessons/10_Turtles/70_Projects/10_LeagueBot.py
+++ lessons/10_Turtles/70_Projects/10_LeagueBot.py
@@ -1,12 +1,12 @@
-""" Leaguebot
+""" 
+LeagueBot
 
 Write your own turtle program! Here is what your program should do
 
-1) Change the turtle image to 'leaguebot_bolt.gif'
+1) Change the turtle image to 'leaguebot_bot.gif'
 2) Change the turtle size to 10x10
 3) Change the turtle line color to 'blue'
 4) Draw a hexagon using a loop and variables. 
-
 """
 
 import turtle as turtle
