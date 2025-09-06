78b573d3b587b134837791db1c7fdfc2fca6bb97
dotnetdork <jaysausa@student.purdueglobal.edu>
2025-09-06 11:21:55 -0400

    Removed unnecessary spaces

--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -7,9 +7,7 @@ directory ) and when you click on it, it moves to a random location on the scree
 
 Use this code to get a random x and y location
 
-
     import random
     x = random.randint(-300, 300)
     y = random.randint(-300, 300)
-
 """
