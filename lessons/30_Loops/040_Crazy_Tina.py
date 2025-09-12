686b0a402a3bff1e1f6e9efe974f54bb4b3b2931
Jay Sausa <jaysausa@student.purdueglobal.edu>
2025-09-11 23:20:14 -0400

    removed spaces

--- lessons/30_Loops/040_Crazy_Tina.py
+++ lessons/30_Loops/040_Crazy_Tina.py
@@ -1,5 +1,4 @@
 """
-
 Create a program that will draw a crazy pattern using the turtle.
 
 Create lists for the path that Tina will take, the angles 
@@ -9,9 +8,7 @@ lists to draw the pattern.
 hint: all of your lists should have the same number of elements.
 Review the ' Using Lists' section of the previous lesson if you need 
 more help
-
 """
-
 
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup(600,600,0,0)               # Set the size of the window
@@ -20,7 +17,6 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
 tina.speed(2)                           # Make the turtle move as fast, but not too fast. 
-
 
 forwards = [ ... ]
 lefts = [ ... ]
@@ -32,10 +28,8 @@ for  i in range(8):
     left = ...
     color = ...
 
-
     tina.color(color)
     tina.forward(forward)
     tina.left(left)
 
 turtle.exitonclick()  
-
