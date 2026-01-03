e12bac09277a89b4f673737e34de5df603bcd220
yl001coder <stackhill@gmail.com>
2026-01-03 00:57:53 +0000

    hell is forever wether you like it or not, had their chance ot behave better now they boil in the pot, cause the rules are black and white there's no use in tryna fight it, they're burning for their lives until we kill em again

--- lessons/30_Loops/070_List_Story.py
+++ lessons/30_Loops/070_List_Story.py
@@ -5,10 +5,11 @@ Use indexing to get words from the list, then
 append them to the story
 """
 
-words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
-    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
+words = ['Once ', '👦 ', 'upon ', '🐕 ' , 'park ', 'met ', 'with ' , 'a ', 'the ', 
+    'time ', 'to ', 'who ', '🐈 ', '👧 ' , 'and ', 'went ', 'had ', 'play ', '⚽.', 'they ']
 
-story = []
+story = [words[0],words[2],words[7],words[9],words[7],words[13],words[15],words[10],words[8],words[4],
+         words[10],words[17]]
 
 # Create a story using the words in the list
 
