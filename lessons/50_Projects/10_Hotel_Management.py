595963ec2f7b0a6f4112c9860a2d11584dfea07e
yl001coder <stackhill@gmail.com>
2026-04-03 23:54:51 +0000

    dear evan hansen we've been way too out of touch things have been crazy and it sucks taht we don't talk that much

--- lessons/50_Projects/10_Hotel_Management.py
+++ lessons/50_Projects/10_Hotel_Management.py
@@ -31,55 +31,33 @@ guest_book = dict()
 days = int()
 menu = ["jellybears", "strawberry cones", "crystal jellies","omurice"
         "cake roll"]
+name_night = dict()
+guest = list()
 
 def main():
     #main function
     while(hotel_is_open == True):
 
-        welcome()
+        hotel_stuff()
 
-def welcome():
-    name()
-    num_of_nights()
-    room_numbers()
-    room_service()
-
-def name():
+def hotel_stuff():
     """
-    returns: name
+    does hotel stuff 
+    
+    """
+    welcomes()
+
+    room_service("Wind Archer Cookie")
+    time_pass()
+
+def welcomes():
+    """
+    returns: gets name, #of nights, #of rooms
 
     Functionality:
     asks guest for their name (to store it in a guestbook/dictionary)
     and asks them for the number of rooms that they want to check into
 
-    """
-
-    if(len(rooms)==0):
-        return None
-    #welcomes guests, and helps them check-in
-    print("Hi, I'm Gingerbrave, and welcome to the Earthbread Hotel!")
-    name = input("What is your name? ")
-    if(name == "dark enchantress cookie"):
-        print("!!ANNOUNCEMENT!! /n" \
-        "we are currently shutting down the Earthbread Hotel /n" \
-        "due to safety reasons. /n" \
-        "Thank you!")
-
-        hotel_is_open == False
-
-    
-    no_of_rooms = True
-    while(no_of_rooms == True):
-        room_numbers = input(name + ", how many rooms would you like to check into? ")
-        if(int(room_numbers)>len(rooms)):
-            print("Unfortunately, we don't have that many open rooms at the moment.")
-        else:
-            no_of_rooms = False
-    
-    return name
-    
-def num_of_nights():
-    """
     returns: 
     number of nights the guest is staying
 
@@ -89,48 +67,102 @@ def num_of_nights():
 
     """
 
+    if(len(rooms)==0):
+        return None
+    #welcomes guests, and helps them check-in
+    print("Hi, I'm Gingerbrave, and welcome to the Earthbread Hotel!")
+    name = input("What is your name? ")
+    
+    no_of_rooms = True
+
     nights = input("And, how many nights will you be staying? ")
     guest_book[name] = nights
-    return nights
+    name_night[name] = days
+ 
+ 
+    while(no_of_rooms == True):
+        roooms = input("Additionally, how many rooms would you like to book? ")
+        if(int(roooms)>len(rooms)):
+            print("Unfortunately, we don't have that many open rooms at the moment.")
+        else:
+            no_of_rooms = False
 
-def room_numbers():
-    """"
-    returns:
-    none (because the rooms/rooms taken will be stored in a list)
 
-    functionality"
-    gives the guest a certain amount of room numbers based off
-    of how many rooms they chose to check into earlier
-
-    """
-
-    print("Your room numbers will be: ")
-    for i in range(int(room_numbers)):
+    print("Alright. Your room numbers will be: ")
+    for i in range(int(roooms)):
         print(rooms[0])
         rooms_taken.append(rooms[0])
         rooms.remove(rooms[0])
 
-def room_service(name):
-    """
-    room service function for fun that will be implemented later
+    return nights, name
 
-    returns:
-    what the guest wants to eat from room service
+def want_fud(guest):
+    """
+    ask guest if want food
+
+    returns: Y/N
     
     """
+
+    foodYN = input(guest+ ", would you like some food now that you've checked in? (yes/no input) ")
+    if(foodYN.lower == "yes"):
+        return True
+    elif():
+        return False
+
+def room_service(name):
+    """
+    room service function for fun later
+
+    returns:
+    what the guest wants to eat
+    
+    """
+    fuds = input("Food! (press enter)")
     print("Here's the menu:")
-    for i in range(5):
+    for i in range(4):
         print(menu[i])
 
-    food = input(name + "What would you like to eat?")
+    food = input(name + ", what would you like to eat? ")
     l = menu.split()
     for i in l:
-        if(i in menu):
+        if(food in menu):
+            print("Here is your " + food)
             return food
     return False
 
+def time_pass():
+    """ 
+    function: to keep track of days to check guests out and in
+    runs functions for each day 
+
+    iwfilwyoaoaidchwownmhlibym
     
+    i
+
+    """
+    days = 1
+    welcomes()
+    room_service(guest[0])
+
+def check_out(guest):
+    """
+    function:
+    checks-out guests by seeing when
+    int(name_night[name]) - int(guest_book[name]) = 0
+    to check them out
+
+    guests == the guest list
+    
+    """
+    for i in len(guest):
+        if((name_night[guest[i]]) - int(guest_book[guest[i]]) == 0):
+            print("Thank you for staying " + guest + "!")
+            del guest_book[str(guest)]
+        
+"""
 
 
+"""
 
 main()
